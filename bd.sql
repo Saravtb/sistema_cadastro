4d9dcb691dacfb0da64ed5c945caf34ed7b2d4ec
Valmir G. Jesus <vj.prof01@gmail.com>
2024-06-05 11:47:21 -0300

    Comentários no SQL
    Incluir comentários que explicam cada expressão SQL no script

--- bd.sql
+++ bd.sql
@@ -1,12 +1,17 @@
+-- Expressçao SQL para criar banco de dados
 CREATE DATABASE sistema_cadastro;
+
+-- Expressão SQL para informar à IDE que este é o banco que estará em uso.
 USE sistema_cadastro;
 
+-- Expressão SQL para criar a tabela de usuários
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     usuario VARCHAR(50) NOT NULL,
     senha VARCHAR(255) NOT NULL
 );
 
+-- Expressão SQL para criar a tabela de formecedores
 CREATE TABLE fornecedores (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
@@ -14,6 +19,7 @@ CREATE TABLE fornecedores (
     telefone VARCHAR(20)
 );
 
+-- Expressão SQL para criar a tabela de produtos relacionada via FK com a tabela de fornecedores
 CREATE TABLE produtos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     fornecedor_id INT,
@@ -23,4 +29,5 @@ CREATE TABLE produtos (
     FOREIGN KEY (fornecedor_id) REFERENCES fornecedores(id)
 );
 
+-- Expressão SQL para cadastrar um usuário
 INSERT INTO usuarios (usuario, senha) VALUES ('admin', MD5('admin123'));
