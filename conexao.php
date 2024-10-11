bfa02faf1dee3ebb6fe96d654f7bcdde6e5d38fa
Jao <dasilvajao639@gmail.com>
2024-10-11 09:40:15 -0300

    - Adaptação do projeto para template.

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $dbname = "sistema_cadastro";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
